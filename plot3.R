b201eac309319c6912c73696a21705f81053dca4
Michael Yang <mike@captain-refactor.co.za>
2014-06-22 20:51:31 +0200

    update plot3 with title

--- plot3.R
+++ plot3.R
@@ -6,6 +6,7 @@ baltimore_pm25 <- NEI[NEI$fips==24510,]
 selected_columns <- melt(baltimore_pm25[, c('Emissions', 'type', 'year')], id=c('year', 'type'))
 clean_data <- dcast(h, year + type ~ variable, sum)
 
-qplot(year, Emissions, data = clean_data, facets = . ~ type,
-      geom=c('point', 'smooth'), method='lm')
+g <- qplot(year, Emissions, data = clean_data, facets = . ~ type,
+           geom=c('point', 'smooth'), method='lm')
+g + labs(title = 'Emission Types Comparison in Baltimore City from 1999 - 2008')
 ggsave(file="plot3.png", dpi=72)
